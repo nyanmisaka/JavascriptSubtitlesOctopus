0d3ffb7f5e55d92fe95cc1ec3d8643c61666f95d
Oneric <oneric@oneric.stub>
2022-12-04 17:47:58 +0100

    build: speed up license extraction
    Apparently licensecheck has a high startup cost, so merging invocations
    promises a neat speedup. However, currently some "\x{....} cannot be
    represented as ascii" error messages appear in our logs. If that happens
    licensecheck exits with 255, which omits all following files of the
    same invocation and also prompts `find -exec .. {} +` or `xargs` to not
    spawn any more invocations. If we were to simply merge invocations by
    one of those means it would result in an incomplete COPYRIGHT file.
    
    Those encoding errors appear to be due to an ASCII locale being set in
    the container, so override LC_ALL to an UTF-8 one. To further make this
    bit more resilient ensure licensecheck errors are no longer ignored by
    capturing and processing its exit code.
    
    Cherry-picked from: https://github.com/libass/JavascriptSubtitlesOctopus/commit/cf04e0361ad9111b3b2f81defb25369e65c8eb52

--- run-docker-build.sh
+++ run-docker-build.sh
@@ -8,7 +8,7 @@ if [ "$FAST" -eq 0 ] ; then
     docker build -t "$IMAGE" .
 fi
 if [ "$#" -eq 0 ] ; then
-    docker run -it --rm -v "${PWD}":/code --name "$CONTAINER" "$IMAGE":latest
+    docker run -it --rm --env LC_ALL=C.UTF-8 -v "${PWD}":/code --name "$CONTAINER" "$IMAGE":latest
 else
-    docker run -it --rm -v "${PWD}":/code --name "$CONTAINER" "$IMAGE":latest "$@"
+    docker run -it --rm --env LC_ALL=C.UTF-8 -v "${PWD}":/code --name "$CONTAINER" "$IMAGE":latest "$@"
 fi
